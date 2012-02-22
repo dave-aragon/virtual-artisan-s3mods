34de13a6afc2e493cc5568541535555f5099f46f
jaana.nau <jaana.nau@gmail.com>
2012-02-22 16:42:39 +0000

    New version of the dancer

---
+++ DancersSpot/DancersSpot/ExoticDancer.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sims3.Gameplay.CAS;
+using Sims3.Gameplay.Interfaces;
+using Sims3.Gameplay.Actors;
+
+using Sims3.Gameplay.Core;
+using Sims3.Gameplay.Interactions;
+using System.Diagnostics;
+using Misukisu.Common;
+using Sims3.Gameplay.Objects.Misukisu;
+using Sims3.Gameplay.Skills;
+
+namespace Sims3.Gameplay.Roles.Misukisu
+{
+
+
+    public class ExoticDancer : Pianist
+    {
+
+        public ExoticDancer()
+            : base()
+        { }
+
+        public ExoticDancer(RoleData data, SimDescription s, IRoleGiver roleGiver)
+            : base(data, s, roleGiver)
+        { }
+
+        public new void RemoveSimFromRole()
+        {
+            //Message.Show("Sim is removed from role " + new StackTrace().ToString());
+            base.RemoveSimFromRole();
+        }
+
+        public static ExoticDancer clone(Role toClone, Sim simInRole)
+        {
+            SimDescription actor = null;
+            List<SimDescription> townies = Household.AllTownieSimDescriptions();
+            foreach (SimDescription townie in townies)
+            {
+                if (simInRole == townie.CreatedSim)
+                {
+                    actor = townie;
+                    //Message.Show("Clone found the actor");
+                    break;
+                }
+            }
+
+            if (actor == null)
+            {
+                List<SimDescription> residents = Household.AllSimsLivingInWorld();
+                foreach (SimDescription townie in residents)
+                {
+                    if (simInRole == townie.CreatedSim)
+                    {
+                        actor = townie;
+                        //Message.Show("Clone found the actor");
+                        break;
+                    }
+                }
+            }
+
+            ExoticDancer newRole = null;
+            if (actor != null)
+            {
+                newRole = new ExoticDancer(toClone.Data, actor, toClone.RoleGivingObject);
+                newRole.StartRole();
+            }
+            return newRole;
+        }
+
+
+        public override void SimulateRole(float minPassed)
+        {
+            if (base.IsActive)
+            {
+                Sim createdSim = base.mSim.CreatedSim;
+                if (((createdSim != null) && (base.RoleGivingObject != null)) && ((createdSim.LotCurrent == base.RoleGivingObject.LotCurrent) && !this.IsOnBreak()))
+                {
+                    base.RoleGivingObject.PushRoleStartingInteraction(createdSim);
+                }
+            }
+
+        }
+
+        protected override void EndRole()
+        {
+            //Message.Show("Custom role ending " + new StackTrace().ToString());
+            base.EndRole();
+            //if (this.mSim.CreatedSim != null)
+            //{
+            //    this.mSim.CreatedSim.SwitchToPreviousOutfitWithoutSpin();
+            //}
+        }
+
+        protected override void SwitchIntoOutfit()
+        {
+            //Message.Show("Switching into outft");
+            try
+            {
+                this.mSim.CreatedSim.InteractionQueue.CancelAllInteractions();
+                Lot.MetaAutonomyType venueType = base.RoleGivingObject.LotCurrent.GetMetaAutonomyType;
+                Sim sim = this.mSim.CreatedSim;
+                SwitchToProperClothing(venueType, sim);
+
+            }
+            catch (Exception e)
+            {
+                Message.ShowError(DancersStage.NAME, "Cannot change sim's clothes", false, e);
+            }
+
+        }
+
+        public static void SwitchToProperClothing(Lot.MetaAutonomyType venueType, Sim sim)
+        {
+            SimIFace.CAS.OutfitCategories outfitType = SimIFace.CAS.OutfitCategories.Everyday;
+            if (venueType == Lot.MetaAutonomyType.CocktailLoungeAsian || venueType == Lot.MetaAutonomyType.CocktailLoungeCelebrity || venueType == Lot.MetaAutonomyType.CocktailLoungeVampire)
+            {
+                outfitType = SimIFace.CAS.OutfitCategories.Formalwear;
+            }
+
+            sim.SwitchToOutfitWithoutSpin(Sim.ClothesChangeReason.GoingToWork, outfitType);
+        }
+
+        protected override void StartRole()
+        {
+            //Message.Show("starting new custom role ");
+            try
+            {
+                if (!this.mIsActive && this.mSim.IsValidDescription)
+                {
+                    InstantiateSim();
+                    this.SwitchIntoOutfit();
+                    if (this.mSim.CreatedSim != null)
+                    {
+                        AddNeededMotives();
+
+                        if (base.RoleGivingObject != null)
+                        {
+                            this.mIsActive = true;
+                            MakeSimComeToRoleLot();
+
+                            if (base.mSim.CreatedSim != null)
+                            {
+                                base.mRoleGivingObject.AddRoleGivingInteraction(base.mSim.CreatedSim);
+                            }
+
+
+                            if (this.RoleGivingObject != null)
+                            {
+                                this.RoleGivingObject.PushRoleStartingInteraction(this.mSim.CreatedSim);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Message.ShowError(DancersStage.NAME, "Cannot start the role", false, e);
+            }
+        }
+
+        private void AddNeededMotives()
+        {
+            try
+            {
+                SkillManager skillManager = base.mSim.SkillManager;
+                if (skillManager != null)
+                {
+                    DancingSkill skill = skillManager.GetSkill<DancingSkill>(SkillNames.ClubDancing);
+                    if (skill == null)
+                    {
+                        skillManager.AddAutomaticSkill(SkillNames.ClubDancing);
+                        skill = skillManager.GetSkill<DancingSkill>(SkillNames.ClubDancing);
+                    }
+                    
+                    if (skill != null)
+                    {
+                        skill.SkillLevel = skill.MaxSkillLevel;
+                    }
+                    else
+                    {
+                        Message.Show("Could not add dancing skills to this sim");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Message.ShowError(DancersStage.NAME, "Cannot add skills to dancer", false, e);
+            }
+        }
+
+        public override string CareerTitleKey
+        {
+            get
+            {
+                return "Gameplay/Objects/Miscellaneous/VelvetRopes:BouncerRoleName";
+            }
+        }
+
+
+        public void MakeSimComeToRoleLot()
+        {
+            Lot roleLot = base.RoleGivingObject.LotCurrent;
+            if ((this.mSim.CreatedSim.LotCurrent == null) || !(this.mSim.CreatedSim.LotCurrent == roleLot))
+            {
+                this.mSim.CreatedSim.InteractionQueue.Add(Sim.GoToLotThatSatisfiesMyRole.Singleton.CreateInstance(this.mSim.CreatedSim, this.mSim.CreatedSim, new InteractionPriority(InteractionPriorityLevel.High), true, true));
+                //Message.Show("Called sim to arrive");
+            }
+        }
+
+        public void InstantiateSim()
+        {
+            if (this.mSim.CreatedSim == null)
+            {
+                Lot lot = LotManager.SelectRandomLotForNPCMoveIn(null);
+                this.mSim.CreatedSim = this.mSim.Instantiate(lot);
+                //Message.Show("Sim instantiated: " + SimInRole);
+            }
+
+        }
+
+    }
+
+}
---
+++ DancersSpot/DancersSpot/PerformShow.cs
@@ -0,0 +1,238 @@
+﻿using Sims3.Gameplay.Interactions;
+using Sims3.Gameplay.Actors;
+using Sims3.Gameplay.Objects.Counters;
+using Sims3.SimIFace;
+using Sims3.SimIFace.Enums;
+using Sims3.Gameplay.Abstracts;
+using Sims3.Gameplay.Autonomy;
+using System.Collections.Generic;
+using Sims3.Gameplay.ActorSystems;
+using Sims3.Gameplay.Skills;
+using Sims3.Gameplay.EventSystem;
+using Sims3.Gameplay;
+using Sims3.Gameplay.Utilities;
+using System;
+using Sims3.Gameplay.Objects;
+using Sims3.Gameplay.Interfaces;
+using Sims3.Gameplay.Objects.Misukisu;
+using Misukisu.Common;
+
+namespace Misukisu.Sims3.Gameplay.Interactions
+{
+    public class PerformShow : Interaction<Sim, DancersStage>
+    {
+        // Fields
+        private Slot mEnterSlot;
+       
+        private GameObject mReservedTile;
+        public static readonly InteractionDefinition Singleton = new Definition();
+        public bool Watchable = true;
+
+        // Methods
+        public override void Cleanup()
+        {           
+            if (this.mReservedTile != null)
+            {
+                this.mReservedTile.Destroy();
+                this.mReservedTile = null;
+            }
+            base.Cleanup();
+        }
+
+        private int ClosestRoutingSlotFirstComparer(Slot first, Slot second)
+        {
+            GameObject target = base.Target as GameObject;
+            Vector3 position = base.Actor.Position;
+            Vector3 positionOfSlot = target.GetPositionOfSlot(first);
+            Vector3 vector3 = target.GetPositionOfSlot(second);
+            float num = (positionOfSlot - position).LengthSqr();
+            float num2 = (vector3 - position).LengthSqr();
+            return num.CompareTo(num2);
+        }
+
+        private void DanceLoop(StateMachineClient smc, InteractionInstance.LoopData ld)
+        {
+            int skillLevel = base.Actor.SkillManager.GetSkillLevel(SkillNames.ClubDancing);
+            if (skillLevel > 1)
+            {
+                base.SetParameter("ClubDanceSkill", (DanceExpertise)Math.Max(0, skillLevel - 1));
+            }
+            if (!base.Actor.BuffManager.HasElement(BuffNames.HasToPee))
+            {
+                base.Actor.AddExitReason(ExitReason.Finished);
+            }
+            
+            TimePassedOnLotEvent e = new TimePassedOnLotEvent(EventTypeId.kSimDanced, base.Actor, base.Target, ld.mDeltaTime, base.Target.LotCurrent.LotId);
+            EventTracker.SendEvent(e);
+        }
+
+        private static bool DanceSpotObstructed(IDanceOnCounterOrTableObject danceObject)
+        {
+            if (danceObject is TableBar)
+            {
+                if (danceObject.UseCount > 0)
+                {
+                    return true;
+                }
+            }
+            else if (danceObject.InUse)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static bool ObjectHasSpaceAboveIt(GameObject gameObj)
+        {
+            LotLocation location = new LotLocation();
+            if (World.GetLotLocation(gameObj.Position, ref location) == 0L)
+            {
+                return false;
+            }
+            foreach (ObjectGuid guid in World.GetObjects(gameObj.LotCurrent.LotId, location))
+            {
+                IScriptProxy proxy = Simulator.GetProxy(guid);
+                if (proxy != null)
+                {
+                    GameObject target = proxy.Target as GameObject;
+                    if (target != null)
+                    {
+                        if (target is Sim)
+                        {
+                            return false;
+                        }
+                        if ((target.CheckObjectPlacement(UserToolUtils.BuildBuyProductType.Ceiling) || target.CheckObjectPlacement(UserToolUtils.BuildBuyProductType.Wall)) && (!(target is IPainting) && !(target is Windows)))
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        protected override bool Run()
+        {
+            Message.Show("Show is starting");
+            List<Slot> danceOnRoutingSlots = base.Target.GetDanceOnRoutingSlots(base.Actor);
+            danceOnRoutingSlots.Sort(new Comparison<Slot>(this.ClosestRoutingSlotFirstComparer));
+            for (int i = 0; i < danceOnRoutingSlots.Count; i++)
+            {
+                Slot slotName = danceOnRoutingSlots[i];
+                Route r = base.Actor.CreateRoute();
+                r.DoRouteFail = i == (danceOnRoutingSlots.Count - 1);
+                r.PlanToPoint(base.Target.GetSlotPosition(slotName));
+                if (base.Actor.DoRoute(r) && base.Actor.RouteTurnToFace(base.Target.Position))
+                {
+                    this.mEnterSlot = slotName;
+                    break;
+                }
+            }
+            if (this.mEnterSlot == Slot.None)
+            {
+                return false;
+            }
+            if (DanceSpotObstructed(base.Target))
+            {
+                return false;
+            }
+            Vector3 slotPosition = base.Target.GetSlotPosition(this.mEnterSlot);
+            Vector3 forwardOfSlot = base.Target.GetForwardOfSlot(this.mEnterSlot);
+            this.mReservedTile = GlobalFunctions.CreateObject("DynamicFootprintPlacement", slotPosition, 0, forwardOfSlot) as GameObject;
+        
+            base.StandardEntry();
+           
+            base.EnterStateMachine("danceOnCounterAndTable", "Enter", "x", "counter");
+            int skillLevel = base.Actor.SkillManager.GetSkillLevel(SkillNames.ClubDancing);
+            base.SetParameter("ClubDanceSkill", (DanceExpertise)Math.Max(0, skillLevel - 1));
+            bool slotIsBackSide = false;
+            bool paramValue = base.Target.IsIslandCounter(this.mEnterSlot, ref slotIsBackSide);
+            base.SetParameter("IsIslandCounter", paramValue);
+            base.SetParameter("IsIslandCounterBack", slotIsBackSide);
+            base.Actor.AddInteraction(WatchSimDancingOnCounterOrTable.Singleton);
+            base.BeginCommodityUpdates();
+            base.AddSynchronousOneShotScriptEventHandler(0x65, new SacsEventHandler(this.SnapSimToTop));
+            base.Actor.LookAtManager.SetInteractionLookAtThreshold(150);
+            base.AnimateSim("Dance");
+            bool succeeded = this.DoLoop(~(ExitReason.Replan | ExitReason.MidRoutePushRequested | ExitReason.ObjectStateChanged | ExitReason.PlayIdle | ExitReason.MaxSkillPointsReached), 
+                new InteractionInstance.InsideLoopFunction(this.DanceLoop), base.mCurrentStateMachine);
+            if (this.mReservedTile != null)
+            {
+                foreach (uint num3 in this.mReservedTile.GetFootprintNameHashes())
+                {
+                    this.mReservedTile.PushSimsFromFootprint(num3, base.Actor, null, true);
+                }
+            }
+            base.AddSynchronousOneShotScriptEventHandler(0x66, new SacsEventHandler(this.SnapSimToBottom));
+            base.AnimateSim("GetDown");
+            base.Actor.RemoveInteractionByType(WatchSimDancingOnCounterOrTable.Singleton);
+            this.Watchable = false;
+            base.EndCommodityUpdates(succeeded);
+            Message.Show("Eciting the machine");
+            base.StandardExit();
+            return true;
+        }
+
+        private void SnapSimToBottom(StateMachineClient sender, IEvent evt)
+        {
+            GameObject target = base.Target as GameObject;
+            base.Actor.SnapToSlot(target, this.mEnterSlot);
+        }
+
+        private void SnapSimToTop(StateMachineClient sender, IEvent evt)
+        {
+            Slot danceSlot = mEnterSlot;
+            if (danceSlot != null)
+            {
+                GameObject target = base.Target as GameObject;
+                base.Actor.SnapToSlot(target, danceSlot);
+                Vector3 forwardOfSlot = base.Target.GetForwardOfSlot(this.mEnterSlot);
+                base.Actor.SetForward(-forwardOfSlot);
+            }
+        }
+
+        // Nested Types
+        private sealed class Definition : InteractionDefinition<Sim, DancersStage, PerformShow>
+        {
+
+            protected override string GetInteractionName(Sim a, DancersStage target, InteractionObjectPair interaction)
+            {
+                return target.GetDanceOnObjectInteractionLocalizedString();
+            }
+
+            private static string greyedOutTooltipCallbackForObjectAbove()
+            {
+                return Localization.LocalizeString("Gameplay/Objects/DanceOnCounterOrTable:ObjectAbove", new object[0]);
+            }
+
+            private static string greyedOutTooltipCallbackForSpotObstructed()
+            {
+                return Localization.LocalizeString("Gameplay/Objects/DanceOnCounterOrTable:Blocked", new object[0]);
+            }
+
+            protected override bool Test(Sim sim, DancersStage target, bool isAutonomous, ref GreyedOutTooltipCallback greyedOutTooltipCallback)
+            {
+
+                if (!PerformShow.ObjectHasSpaceAboveIt(target as GameObject))
+                {
+                    greyedOutTooltipCallback = new GreyedOutTooltipCallback(PerformShow.Definition.greyedOutTooltipCallbackForObjectAbove);
+                    return false;
+                }
+                if (PerformShow.DanceSpotObstructed(target))
+                {
+                    greyedOutTooltipCallback = new GreyedOutTooltipCallback(PerformShow.Definition.greyedOutTooltipCallbackForSpotObstructed);
+                    return false;
+                }
+
+                if (isAutonomous)
+                {
+                    return false;
+                }
+
+                
+                return true;
+            }
+        }
+    }
+}
