1626fb3f87a4671bf044956ec304663b41e9238b
jaana.nau@gmail.com <jaana.nau@gmail.com@32cca1d3-84ab-2be4-a5c5-6ae9ee8143ed>
2012-02-22 16:42:39 +0000

    New version of the dancer

---
+++ trunk/DancersSpot/DancersSpot/DancersStage.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Misukisu.Common;
+using Sims3.Gameplay.Abstracts;
+using Sims3.Gameplay.Actors;
+using Sims3.Gameplay.Autonomy;
+using Sims3.Gameplay.CAS;
+using Sims3.Gameplay.Core;
+using Sims3.Gameplay.Interactions;
+using Sims3.Gameplay.Interfaces;
+using Sims3.Gameplay.Roles;
+using Sims3.Gameplay.Roles.Misukisu;
+using Sims3.Gameplay.Skills;
+using Sims3.Gameplay.Utilities;
+using Sims3.SimIFace;
+using Sims3.UI;
+using Sims3.Gameplay.Objects.Counters;
+using Misukisu.Sims3.Gameplay.Interactions;
+using Sims3.Gameplay.Objects.Tables.Mimics;
+
+namespace Sims3.Gameplay.Objects.Misukisu
+{
+    public class DancersStage : TableBarPubRound, IRoleGiverExtended, IRoleGiver
+    {
+        public static string NAME = "Dancer's Stage";
+
+        private Roles.Role mCurrentRole;
+        private float mStartTime = 0F;
+        private float mEndTime = 0F;
+        //private float[] mShowtimes;
+
+
+        public override void OnStartup()
+        {
+            base.OnStartup();
+            base.AddInteraction(TuneExoticDancer.Singleton);
+            base.AddInteraction(PerformShow.Singleton);
+        }
+
+        public void TuningChanged(float startTime, float endTime)
+        {
+            mStartTime = startTime;
+            mEndTime = endTime;
+            ResetRole();
+        }
+
+        private void ResetRole()
+        {
+            EndRoleAndReplaceWithNew(CurrentRole);
+        }
+
+        public void GetRoleTimes(out float startTime, out float endTime)
+        {
+            startTime = mStartTime;
+            endTime = mEndTime;
+
+            //Message.Show("Someone is asking role times " + new StackTrace().ToString());
+            if (startTime == 0)
+            {
+                if (base.LotCurrent != null)
+                {
+                    if (Bartending.TryGetHoursOfOperation(base.LotCurrent, ref startTime, ref endTime))
+                    {
+                        mStartTime = startTime;
+                        mEndTime = endTime;
+                        //Message.Show("Setting relative role times from " + startTime + " to " + endTime);
+                    }
+                    else
+                    {
+                        //Message.Show("Setting fixed role times");
+                        startTime = 18F;
+                        mStartTime = startTime;
+                        endTime = 24F;
+                        mEndTime = endTime;
+                    }
+
+                }
+            }
+            else
+            {
+                //Message.Show("Role time is from " + startTime + " to " + endTime);
+            }
+
+        }
+
+        public void AddRoleGivingInteraction(Actors.Sim sim)
+        {
+
+            Message.Show("Role started");
+            //try
+            //{
+
+            //    Message.Show("Adding role actions to " + (sim != null ? sim.FullName : "null"));
+            //}
+            //catch (Exception ex)
+            //{
+            //    Message.Show("Adding role actions to null " + ex.Message + " - " + new StackTrace().ToString());
+            //}
+        }
+
+
+
+        public Roles.Role CurrentRole
+        {
+            get
+            {
+                return this.mCurrentRole;
+            }
+            set
+            {
+                ExoticDancer newRole = value as ExoticDancer;
+                if (newRole != null)
+                {
+                    this.mCurrentRole = newRole;
+                }
+                else if (value != null)
+                {
+                    EndRoleAndReplaceWithNew(value);
+                }
+                else
+                {
+                    //Message.Show("Null role was set " + new StackTrace().ToString());
+                    this.mCurrentRole = value;
+                }
+
+            }
+
+        }
+
+        private void EndRoleAndReplaceWithNew(Role value)
+        {
+            if (value != null)
+            {
+                try
+                {
+                    Sim currentActor = value.SimInRole;
+                    if (currentActor != null)
+                    {
+                        value.RemoveSimFromRole();
+                        ExoticDancer aRole = ExoticDancer.clone(value, currentActor);
+
+                        if (aRole != null)
+                        {
+                            this.mCurrentRole = aRole;
+                            RoleManager.sRoleManager.AddRole(aRole);
+                        }
+                        else
+                        {
+                            Message.ShowError(DancersStage.NAME, "Cannot create custom role, clone failed", true, null);
+
+                        }
+
+
+                    }
+                    else
+                    {
+                        Message.ShowError(DancersStage.NAME, "Cannot create custom role, Pianist sim not instantiated", true, null);
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Message.ShowError(DancersStage.NAME, "Cannot create custom role", true, ex);
+                    this.mCurrentRole = value;
+                }
+            }
+        }
+
+        public void PushRoleStartingInteraction(Actors.Sim sim)
+        {
+            try
+            {
+                if (sim != null)
+                {
+                    Message.Show("Pushing sim to perform");
+                    InteractionInstance instance = PerformShow.Singleton.CreateInstance(this, sim,
+                        new InteractionPriority(InteractionPriorityLevel.RequiredNPCBehavior), true, false);
+                    sim.InteractionQueue.AddAfterCheckingForDuplicates(instance);
+                }
+            }
+            catch (Exception ex)
+            {
+                Message.ShowError(DancersStage.NAME, "Sim cannot play the role", false, ex);
+            }
+        }
+
+        public void RemoveRoleGivingInteraction(Actors.Sim sim)
+        {
+
+        }
+
+        public string RoleName(bool isFemale)
+        {
+            return "Exotic Dancer";
+        }
+
+        public Roles.Role.RoleType RoleType
+        {
+            get { return Role.RoleType.Pianist; }
+        }
+    }
+}
---
+++ trunk/DancersSpot/DancersSpot/TuneDancer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sims3.Gameplay.Actors;
+using Sims3.Gameplay.Interactions;
+using Misukisu.Common;
+using Sims3.Gameplay.Autonomy;
+using Sims3.UI;
+using Sims3.SimIFace;
+using Sims3.Gameplay.Objects.Misukisu;
+
+namespace Misukisu.Sims3.Gameplay.Interactions
+{
+
+    public class TuneExoticDancer : Interaction<Sim, DancersStage>
+    {
+        public static readonly InteractionDefinition Singleton = new Definition();
+
+        protected override bool Run()
+        {
+            try
+            {
+
+                float startTime;
+                float endTime;
+                base.Target.GetRoleTimes(out startTime, out endTime);
+
+                float start;
+                float end;
+                bool ok = ShowTimeTuningDialog(startTime, endTime, out start, out end);
+                if (ok)
+                {
+                    base.Target.TuningChanged(start, end);
+                }
+            }
+            catch (Exception e)
+            {
+                Message.ShowError(DancersStage.NAME, "Tuning failed, please try again", false, e);
+
+            }
+            return true;
+        }
+
+        private static bool ShowTimeTuningDialog(float startTime, float endTime, out float start, out float end)
+        {
+            bool changes = false;
+            string[] values = ThreeStringInputDialog.Show("The Dancer", new string[] { "Arrives at (hour, 0-24, 0='reset')", "Leaves at (hour, 0-24, 0='reset')", "Spare field for future needs" }, new string[] { startTime.ToString(), endTime.ToString(), "" }, true);
+
+            if (!string.IsNullOrEmpty(values[0]))
+            {
+                start = float.Parse(values[0]);
+                changes = true;
+            }
+            else
+            {
+                start = 0;
+            }
+
+            if (!string.IsNullOrEmpty(values[1]))
+            {
+                end = float.Parse(values[1]);
+                changes = true;
+            }
+            else
+            {
+                end = 0;
+            }
+            return changes;
+        }
+
+
+
+        private sealed class Definition : InteractionDefinition<Sim, DancersStage, TuneExoticDancer>
+        {
+
+            protected override string GetInteractionName(Sim a, DancersStage target, InteractionObjectPair interaction)
+            {
+                return "Tuning...";
+            }
+
+            protected override bool Test(Sim a, DancersStage target, bool isAutonomous, ref GreyedOutTooltipCallback greyedOutTooltipCallback)
+            {
+                return !isAutonomous;
+            }
+        }
+    }
+}
