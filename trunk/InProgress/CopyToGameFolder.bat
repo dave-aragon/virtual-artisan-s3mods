71cff60667154449dd63d77aacc7a2ef1b6a2ac4
jaana.nau@gmail.com <jaana.nau@gmail.com@32cca1d3-84ab-2be4-a5c5-6ae9ee8143ed>
2012-03-25 17:13:54 +0000

    Ready?

--- trunk/InProgress/CopyToGameFolder.bat
+++ trunk/InProgress/CopyToGameFolder.bat
@@ -1,2 +1,2 @@
 xcopy /Y /S *.package "C:\Users\kulmala\Documents\Electronic Arts\The Sims 3\Mods\Packages\My Stuff\"
-xcopy /Y /S *.sims3pack "C:\Users\kulmala\Documents\Electronic Arts\The Sims 3\Mods\Packages\My Stuff\"
+xcopy /Y /S *.sims3pack "C:\Users\kulmala\Documents\Electronic Arts\The Sims 3\Downloads\"
