6cbb10b8fc3b9d21f492067caadd93613545c844
jaana.nau@gmail.com <jaana.nau@gmail.com@32cca1d3-84ab-2be4-a5c5-6ae9ee8143ed>
2012-04-06 10:25:06 +0000

    First version of noVampireGlow

--- trunk/Mods/NoVampireGlow/NoVampireGlow/NoVampireGlow.cs
+++ trunk/Mods/NoVampireGlow/NoVampireGlow/NoVampireGlow.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using Sims3.SimIFace;
+﻿using Sims3.SimIFace;
 using System.Collections.Generic;
 using System.Text;
 using Sims3.Gameplay.CAS;
 using Sims3.Gameplay.Utilities;
 using Sims3.UI;
+using System;
+using Sims3.Gameplay.EventSystem;
+using Sims3.Gameplay;
+using Sims3.Gameplay.ActorSystems;
 
 namespace Misukisu.NoVampireGlow
 {
@@ -12,43 +15,71 @@ namespace Misukisu.NoVampireGlow
     {
         [TunableComment("Scripting Mod Instantiator, value does not matter, only its existence"), Tunable]
         protected static bool kInstantiator = false;
-        public static readonly RemoveVampireGlow Singleton= new RemoveVampireGlow();
+        public static readonly RemoveVampireGlow Singleton = new RemoveVampireGlow();
+        public static List<AlarmHandle> timers = new List<AlarmHandle>();
 
         static RemoveVampireGlow()
         {
-            World.OnWorldLoadFinishedEventHandler += new EventHandler(OnWorldLoadFinished);
+            InWorldState.InWorldSubStateChanging += new InWorldState.InWorldSubStateChangingCallback(InWorldSubStateChanged);
         }
 
-        private static void OnWorldLoadFinished(object sender, EventArgs e)
+        public static void InWorldSubStateChanged(InWorldState.SubState previousState, InWorldState.SubState newState)
         {
-            Singleton.RemoveGlowFromAll();
-            AlarmManager.Global.AddAlarmDay(
-                       World.GetSunriseTime() + 1, DaysOfTheWeek.All,
-                       new AlarmTimerCallback(Singleton.RemoveGlowFromAll),
-                       "Vampire Glow Removal", AlarmType.NeverPersisted,Singleton);
+            try
+            {
+                if (newState == InWorldState.SubState.LiveMode)
+                {
+                    AlarmHandle handle = AlarmManager.Global.AddAlarm(3, TimeUnit.Seconds, removeGlow,
+                        "VampireGlowRemoval", AlarmType.NeverPersisted, Singleton);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Just let the show continue
+            }
+        }
+
+        public static void removeGlow()
+        {
+            try
+            {
+                releaseAlarms();
+                Singleton.RemoveGlowFromAll();
+            }
+            catch (Exception ex)
+            {
+                // Just let the show continue
+            }
+        }
+
+        public static void releaseAlarms()
+        {
+            foreach (AlarmHandle handle in timers)
+            {
+                AlarmManager.Global.RemoveAlarm(handle);
+            }
         }
 
         private void RemoveGlowFromAll()
         {
-
             List<SimDescription> residents = Household.AllSimsLivingInWorld();
             List<SimDescription> townies = Household.AllTownieSimDescriptions();
-            RemoveGlowFromVampires(residents);
-            SimpleMessageDialog.Show("Removed Vampire Glow", "From all vamps in world", ModalDialog.PauseMode.NoPause);
-       
+            int count = RemoveGlowFromVampires(residents);
+            //SimpleMessageDialog.Show("Removed Vampire Glow", "From all " + count + " vamps in world", ModalDialog.PauseMode.NoPause);
         }
 
-        private void RemoveGlowFromVampires(List<SimDescription> sims)
+        private int RemoveGlowFromVampires(List<SimDescription> sims)
         {
+            int count = 0;
             foreach (SimDescription sim in sims)
             {
                 if (sim.IsVampire && sim.CreatedSim != null)
                 {
-
                     RemoveGlow(sim);
-                   
+                    count++;
                 }
             }
+            return count;
         }
 
         private void RemoveGlow(SimDescription sim)
@@ -56,6 +87,7 @@ namespace Misukisu.NoVampireGlow
             if (sim.IsVampire && sim.CreatedSim != null)
             {
                 World.ObjectSetVisualOverride(sim.CreatedSim.ObjectId, eVisualOverrideTypes.None, null);
+                sim.OccultManager.UpdateOccultUI();
             }
         }
     }
