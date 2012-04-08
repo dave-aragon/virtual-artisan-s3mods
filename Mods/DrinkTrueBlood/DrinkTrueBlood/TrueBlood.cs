e18f2fd493d30c6a43ca92c27f286609850844b5
jaana.nau <jaana.nau@gmail.com>
2012-04-08 11:48:28 +0000

    TicketMachine and TrueBlood, very first drafts

---
+++ Mods/DrinkTrueBlood/DrinkTrueBlood/TrueBlood.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Misukisu.DrinkTrueBlood;
+
+namespace Sims3.Gameplay.Objects.CookingObjects.Misukisu
+{
+    class TrueBlood : SnackVampireJuice
+    {
+        //private Debugger debugger;
+        public TrueBlood():base(){
+            //debugger = new Debugger(this);
+            //debugger.Debug(this, "Instance of true blood created");
+            //debugger.EndDebugLog();
+        }
+    }
+}
---
+++ Mods/TicketMachine/TicketMachine/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TicketMachine
+{
+    public class Notes
+    {
+        // VenueFlowUtility
+        // MakeSimDoSomethingAfterArrivingAtVenue
+        //  MakeSimLeaveVenueIfNecessary
+    }
+}
