06972ca685819d59fffaf72ddaa1d8e7b6de9fd6
jaana.nau@gmail.com <jaana.nau@gmail.com@32cca1d3-84ab-2be4-a5c5-6ae9ee8143ed>
2012-04-11 14:12:31 +0000

    Version that has cookingprocess, but still not yet get microwaved

--- trunk/Mods/DrinkTrueBlood/DrinkTrueBlood/TrueBlood.cs
+++ trunk/Mods/DrinkTrueBlood/DrinkTrueBlood/TrueBlood.cs
@@ -2,16 +2,53 @@
 using System.Collections.Generic;
 using System.Text;
 using Misukisu.DrinkTrueBlood;
+using Sims3.Gameplay.Utilities;
+using Sims3.Gameplay.Skills;
+using Sims3.Gameplay.Core;
+using Sims3.Gameplay.Objects.Appliances;
+using Sims3.Gameplay.Interactions;
+using Sims3.Gameplay.Objects.FoodObjects;
+using Sims3.Gameplay.ActorSystems;
+using Sims3.Gameplay.Actors;
 
 namespace Sims3.Gameplay.Objects.CookingObjects.Misukisu
 {
-    class TrueBlood : SnackVampireJuice
+    public class TrueBlood : SnackVampireJuice, IMicrowavable
     {
         //private Debugger debugger;
-        public TrueBlood():base(){
+        public TrueBlood()
+            : base()
+        {
+            RecipeReader.InitTrueBloodInstance(this);
             //debugger = new Debugger(this);
             //debugger.Debug(this, "Instance of true blood created");
+
             //debugger.EndDebugLog();
+        }
+
+        
+
+        public override void OnStartup()
+        {
+            base.OnStartup();
+            base.AddInteraction(Snack.Snack_Microwave.Singleton);
+            AddInteraction(TestStatus.Singleton);
+        }
+
+        public override bool CanBeCleanedUp
+        {
+            get
+            {
+                return LotCurrent.IsResidentialLot;
+            }
+        }
+
+        public string ActorNameForMicrowave
+        {
+            get
+            {
+                return "Plate";
+            }
         }
     }
 }
