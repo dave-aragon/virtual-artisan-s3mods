e72be9f509920e7a366b1962ae1e45c78e4d67ec
jaana.nau <jaana.nau@gmail.com>
2012-04-20 05:03:50 +0000

    PosePlayer Addon draft.

---
+++ Mods/SimAddonToPosePlayer/SimAddonToPosePlayer/PoseManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sims3.Gameplay.Objects.CmoPoseBox;
+using Sims3.Gameplay.CAS;
+using Sims3.Gameplay.Actors;
+using Misukisu.SimAddonToPosePlayer;
+
+namespace Misukisu.PosePlayerAddon
+{
+    class PoseManager
+    {
+        public static CmoPoseBox PoseBox = null;
+
+        public PoseManager()
+            : base()
+        {
+            PoseBox = FindPoseBox();
+
+            List<SimDescription> sims = Household.AllSimsLivingInWorld();
+            foreach (SimDescription sim in sims)
+            {
+                Sim aSim = sim.CreatedSim;
+                if (aSim != null)
+                {
+                    aSim.AddInteraction(PoseInteraction.Singleton);
+                    aSim.AddInteraction(StopPosing.Singleton);
+                    aSim.AddInteraction(SnapshotPose.Singleton);
+                }
+            }
+
+        }
+
+        public static CmoPoseBox FindPoseBox()
+        {
+            CmoPoseBox box = null;
+            List<CmoPoseBox> poseboxes = new List<CmoPoseBox>(Sims3.Gameplay.Queries.GetObjects<CmoPoseBox>());
+            if (poseboxes.Count > 0)
+            {
+                box = poseboxes[0];
+            }
+
+            PoseBox = box;
+            return box;
+        }
+    }
+}
