4e2e810cd34f26f72d1d61877cd819a18f938aec
jaana.nau@gmail.com <jaana.nau@gmail.com@32cca1d3-84ab-2be4-a5c5-6ae9ee8143ed>
2012-04-20 05:03:50 +0000

    PosePlayer Addon draft.

---
+++ trunk/Mods/SimAddonToPosePlayer/SimAddonToPosePlayer/AddonLoader.cs
@@ -0,0 +1,50 @@
+﻿
+using Sims3.SimIFace;
+using System;
+using Sims3.Gameplay.EventSystem;
+using System.Reflection;
+namespace Misukisu.PosePlayerAddon
+{
+    public class AddonLoader
+    {
+        private static string POSEBOX = "Sims3.Gameplay.Objects.CmoPoseBox";
+
+        [TunableComment("Scripting Mod Instantiator, value does not matter, only its existence"), Tunable]
+        protected static bool kInstantiator = false;
+
+        static AddonLoader()
+        {
+            World.sOnWorldLoadFinishedEventHandler += new EventHandler(InstallAddon);
+        }
+
+        protected static void InstallAddon(object sender, EventArgs e)
+        {
+            if (IsPosePlayerAvailable())
+            {
+                new PoseManager();
+                EventTracker.AddListener(EventTypeId.kSimInstantiated, new ProcessEventDelegate(OnSimInstantiated));
+            }
+        }
+
+        protected static ListenerAction OnSimInstantiated(Event e)
+        {
+            return ListenerAction.Keep;
+        }
+
+        private static bool IsPosePlayerAvailable()
+        {
+            bool poseboxFound = false;
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (Assembly assembly in assemblies)
+            {
+                string shortName = assembly.FullName.Replace(", Culture=neutral", "");
+                if (POSEBOX.Equals(shortName))
+                {
+                    poseboxFound = true;
+                    break;
+                }
+            }
+            return poseboxFound;
+        }
+    }
+}
---
+++ trunk/Mods/SimAddonToPosePlayer/SimAddonToPosePlayer/StopPosing.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sims3.Gameplay.Interactions;
+using Sims3.Gameplay.Actors;
+using Sims3.Gameplay.Objects.CmoPoseBox;
+using Sims3.Gameplay.Autonomy;
+
+namespace Misukisu.SimAddonToPosePlayer
+{
+    class StopPosing : ImmediateInteraction<Sim, Sim>
+    {
+        public static readonly InteractionDefinition Singleton = new Definition();
+
+        public override bool Run()
+        {
+            Target.AddExitReason(ExitReason.UserCanceled);
+            Target.InteractionQueue.CancelAllInteractionsByType(PlayPoseFromList.Singleton);
+            return true;
+        }
+
+
+        private sealed class Definition : InteractionDefinition<Sim, Sim, StopPosing>
+        {
+            public override bool Test(Sim actor, Sim target, bool isAutonomous, ref Sims3.SimIFace.GreyedOutTooltipCallback greyedOutTooltipCallback)
+            {
+
+                if (target.InteractionQueue.GetCurrentInteraction() is PlayPoseFromList)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            public override string[] GetPath(bool isFemale)
+            {
+                return new String[] { "Posing..." };
+            }
+
+            public override string GetInteractionName(Sim actor, Sim target, InteractionObjectPair iop)
+            {
+                return "Stop Posing";
+            }
+        }
+    }
+}
+
